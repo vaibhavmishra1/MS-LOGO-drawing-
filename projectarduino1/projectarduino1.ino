4cdc4c428bdb13b8dd45c400cd882d527d879201
dhruvsasuke <35889082+dhruvsasuke@users.noreply.github.com>
2018-06-03 01:31:01 +0530

    Committed by Krishna

--- projectarduino1/projectarduino1.ino
+++ projectarduino1/projectarduino1.ino
@@ -8,61 +8,25 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+
 char* comp(){
-    char inData[20];
-    char inChar=-1; // Where to store the character read
- while(Serial.available())
- {
-   inChar = Serial.read(); 
-     byte index = 0;
- 
-     inData[index] = inChar; 
-     index++; 
-    if(inChar=='\n') 
-   {
+  char inData[20];
+  char inChar=-1; // Where to store the character read
+  while(Serial.available()){
+    inChar = Serial.read(); 
+    byte index = 0;
+    inData[index] = inChar; 
+    index++; 
+    if(inChar=='\n') {
      break;
-   }
-    
-    
- }
-
-
-   
+    }
+  }
    index=0;
    return inData;
 }
 
 
 
-class fd()
-{
-       private:
-    char *ch;
-    int num; //distance motor to move forward
-    public:
-    fd()
-    {
-        
-    }
-};
-class bk()
-{
-     char ch[]=comp();
-      int num=ch.toInt(); //distance motor to move backward
-};
-class rt()
-{
-    char ch[]=comp();
-      int num=ch.toInt(); //angle motor move to turn right
-};
-class lt()
-{
-     char ch[]=comp();
-      int num=ch.toInt(); //angle motor move to turn left
-};
-class repeat()
-{
-};
 void loop() { 
         char ch[]=comp();
        if(strcmp(ch,"fd")==0))
