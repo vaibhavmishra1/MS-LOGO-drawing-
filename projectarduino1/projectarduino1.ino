69824ffaf69e0536c1bc192fc8086c899c2cd153
dhruvsasuke <35889082+dhruvsasuke@users.noreply.github.com>
2018-06-03 12:52:47 +0530

    Update projectarduino1.ino

--- projectarduino1/projectarduino1.ino
+++ projectarduino1/projectarduino1.ino
@@ -1,5 +1,41 @@
+#include <Stepper.h>
+#include<math.h>
+#define StepsPerRevolution 200   //Change according to motor
 
 String rc;
+float angle=0pi=3.14;
+float motx,moty;
+
+motx=100*sinf(angle);
+moty=100*cosf(angle);
+
+void radtodeg(){
+  angle=angle*(180/pi);
+}
+
+void degtorad(){
+  angle=angle*(pi/180);
+}
+
+void rt(float x){
+  radtodeg();
+  angle+=x;
+  degtorad();
+}
+
+void lt(floaf x){
+  radtodeg();
+  angle-=x;
+  degtorad();
+}
+
+void fd(float x){
+
+}
+
+void bk(float x){
+  
+}
 
 
 
@@ -7,6 +43,8 @@ void setup() {
     Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
+
+
 
 
 char* comp(){
@@ -50,4 +88,3 @@ void loop() {
                
       }
 }
-
